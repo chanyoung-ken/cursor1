1336161ff03868e1514cf621b45c4cb20d451e3c
chanyoung-ken <chanyoung3351@kentech.ac.kr>
2025-05-15 16:05:50 +0900

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "오늘 뭐 먹지? AI",
   description: "결정장애 미식가를 위한 AI 랜덤슐랭 가이드!",
-  // 추가적인 메타 태그 (예: viewport, icons 등)를 여기에 추가할 수 있습니다.
   viewport: 'width=device-width, initial-scale=1',
   icons: {
-    icon: "/favicon.ico", // public 폴더에 favicon.ico가 있다고 가정
+    icon: "/favicon.ico", 
   },
 };
 
